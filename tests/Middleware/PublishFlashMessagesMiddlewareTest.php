8a7e4bccc3136b7da255649163c91ca514384813
Edo Barnáš <iam@edobarnas.com>
2021-02-12 14:50:46 +0100

    Create PublishFlashMessagesMiddleware unit test

---
+++ tests/Middleware/PublishFlashMessagesMiddlewareTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Responder\Test\Middleware;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Tuzex\Responder\Middleware\PublishFlashMessagesMiddleware;
+use Tuzex\Responder\Result;
+use Tuzex\Responder\Result\FlashMessage;
+use Tuzex\Responder\Result\Payload\PlainText;
+use Tuzex\Responder\Service\FlashMessagePublisher;
+
+final class PublishFlashMessagesMiddlewareTest extends TestCase
+{
+    /**
+     * @dataProvider provideData
+     */
+    public function test(Result $result, int $numberOfFlashMessages): void
+    {
+        $middleware = new PublishFlashMessagesMiddleware(
+            $this->mockFlashMessagePublisher($numberOfFlashMessages)
+        );
+
+        $middleware->execute($result, fn (Result $result): Response => new Response());
+
+        $this->assertTrue(true);
+    }
+
+    public function provideData(): iterable
+    {
+        $data = [
+            'anyone' => [],
+            'one' => [
+                'success' => 'Success!',
+            ],
+            'several' => [
+                'error' => 'Failed!',
+                'success' => 'Success!',
+                'warning' => 'Warning!',
+            ],
+        ];
+
+        foreach ($data as $dataName => $flashMessages) {
+            $result = PlainText::define('');
+            $result->addFlashMessage(
+                ...$this->createFlashMessages($flashMessages)
+            );
+
+            yield $dataName => [
+                'result' => $result,
+                'numberOfFlashMessages' => count($flashMessages),
+            ];
+        }
+    }
+
+    private function mockFlashMessagePublisher(int $numberOfCalls): FlashMessagePublisher
+    {
+        $publisher = $this->createMock(FlashMessagePublisher::class);
+        $publisher->expects($this->exactly($numberOfCalls))
+            ->method('publish');
+
+        return $publisher;
+    }
+
+    private function createFlashMessages(array $definitions): array
+    {
+        $factory = fn (string $type, string $message): FlashMessage => new FlashMessage($type, $message);
+
+        return array_map($factory, array_keys($definitions), $definitions);
+    }
+}
