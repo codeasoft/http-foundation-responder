1b3a70e05d93e2c4af05ca1447b2808d40b75668
Edo Barnáš <iam@edobarnas.com>
2021-02-16 11:34:08 +0100

    Implement FlexResponder by the responder interface

---
+++ src/FlexResponder.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Responder;
+
+use Closure;
+use Symfony\Component\HttpFoundation\Response;
+use Tuzex\Responder\Middleware\CreateResponseMiddleware;
+
+final class FlexResponder implements Responder
+{
+    private Closure $processor;
+
+    public function __construct(CreateResponseMiddleware $middleware)
+    {
+        $this->processor = static fn (Result $result) => null;
+        $this->extend($middleware);
+    }
+
+    public function process(Result $result): Response
+    {
+        return ($this->processor)($result);
+    }
+
+    public function extend(Middleware ...$middlewares): void
+    {
+        $next = $this->processor;
+
+        foreach ($middlewares as $middleware) {
+            $this->processor = $next = static fn (Result $result): Response => $middleware->execute($result, $next);
+        }
+    }
+}
