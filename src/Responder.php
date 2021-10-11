2a1729895098427a3fc713d0a2e803e6edbfdfc2
Edo Barnáš <iam@edobarnas.com>
2021-10-11 19:54:25 +0200

    Derive the Responder interface

--- src/Responder.php
+++ src/Responder.php
@@ -4,32 +4,10 @@ declare(strict_types=1);
 
 namespace Tuzex\Responder;
 
-use Closure;
 use Symfony\Component\HttpFoundation\Response;
-use Tuzex\Responder\Middleware\CreateResponseMiddleware;
 use Tuzex\Responder\Response\ResponseResource;
 
-final class Responder
+interface Responder
 {
-    private Closure $processor;
-
-    public function __construct(CreateResponseMiddleware $createResponseMiddleware)
-    {
-        $this->processor = fn (ResponseResource $responseResource) => null;
-        $this->extend($createResponseMiddleware);
-    }
-
-    public function process(ResponseResource $responseResource): Response
-    {
-        return ($this->processor)($responseResource);
-    }
-
-    public function extend(Middleware ...$middlewares): void
-    {
-        $processor = $this->processor;
-
-        foreach ($middlewares as $middleware) {
-            $this->processor = $processor = fn (ResponseResource $responseResource): Response => $middleware->execute($responseResource, $processor);
-        }
-    }
+    public function process(ResponseResource $responseResource): Response;
 }
